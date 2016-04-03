8327efef62856d2b308cbe9f7a86ca320e1fd40f
Josh Kutsko <Josh Kutsko>
2016-04-03 02:31:57 -0700

    basic internal representation done, no calculations or simulations yet

---
+++ IR/build.py
@@ -0,0 +1,18 @@
+import os
+from Item.py import Item
+
+class Build():
+	#skills_list e.g. [4,4,4,4,5] @ level 21
+	def __init__(hero_name, level, skills_list):
+		pass
+
+	# adds an item to the hero's build
+	def add_item(item_name):
+		pass
+	#gets the stat of the hero by the name of the stat e.g. "damage"
+	def get_stats(stat_name):
+		pass
+	#other_hero is another Build
+	def fights(other_hero):
+
+
